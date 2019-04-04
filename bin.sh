dbd2dbd75584cafa1c20c47a3de76d32738b727f
Agata Schabowska <agata.schabowska@synapse.com>
2019-04-04 15:18:41 +0200

    added script with monitoring execution of proccess

--- bin.sh
+++ bin.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-# declare STRING variable
-STRING="Hello World"
-#print variable on a screen
-echo $STRING
+#running ping in the background
+ping -n 5 google.com &> /dev/null &
+
+while true; do
+    ps -ef | grep ping &> /dev/null
+    #ps - procceses, -ef lists them, grep - if matches ping return 0
+    # if return 1 then the proccess is over
+    if [ $? -eq 1 ]; then
+        echo "Execution is over!"
+        break;
+    fi
+done
+exit 0
+
+
